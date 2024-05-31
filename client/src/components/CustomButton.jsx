e060a0c22eb2d4b06faf0ff46b9bb96affd4f2e6
raafy <raafyshiham@gmail.com>
2024-05-31 14:18:16 +0800

    feat: :sparkles: added custom button component

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import PropTypes from 'prop-types';
+import { useSnapshot } from 'valtio';
+import state from '../store';
 
-function CustomButton() {
+function CustomButton({ title, type, customStyles, handleClick }) {
+  const snap = useSnapshot(state);
+
+  const generateStyle = (type) => {
+    if (type === 'filled') {
+      return {
+        backgroundColor: snap.color,
+        color: '#FFF',
+      };
+    }
+  };
+
   return (
-    
-  )
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
 }
 
-export default CustomButton
+CustomButton.propTypes = {
+  title: PropTypes.string,
+  type: PropTypes.string,
+  customStyles: PropTypes.string,
+  handleClick: PropTypes.func,
+};
+
+export default CustomButton;
