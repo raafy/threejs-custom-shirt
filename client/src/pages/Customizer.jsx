39dbc6cd5d974e3b94f666414c1e6d7948d8d151
raafy <raafyshiham@gmail.com>
2024-06-02 18:03:06 +0800

    update url

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -49,15 +49,18 @@ function Customizer() {
     if (!prompt) return alert('Please enter a prompt!');
     try {
       setGeneratingImg(true);
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        'https://threejs-custom-shirt.onrender.com/api/v1/dalle',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
       const data = await response.json();
       handleDecals(type, `data:image/png;base64,${data.photo}`);
     } catch (error) {
